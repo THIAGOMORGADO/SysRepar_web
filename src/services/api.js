c7e8d3cd46f43bf7e0faafa239265ec5695e624a
thiago morgado <thiago_morgado@hotmail.com>
2022-10-25 12:28:36 -0300

    alterando baseUrl

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,10 @@
 import axios from 'axios';
 
 const server = axios.create({
-  baseURL: 'http://localhost:3333'
+  // baseURL: 'http://localhost:3333'
+},
+  {
+   baseURL: 'server-mu-nine.vercel.app' 
 })
 
 export default server;
